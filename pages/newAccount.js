af74aa94f23acbb2327a2f4b17bff680e9a93cb3
Mauro <molinamauro2@gmail.com>
2020-08-20 19:40:44 -0300

    add form styled component and css in new account component

--- pages/newAccount.js
+++ pages/newAccount.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import { css } from '@emotion/core';
 import Layout from '../components/layout/Layout';
+import { Form, InputContainer, InputSubmit } from '../components/ui/Form';
 
 const NewAccount = () => {
   return (
     <div>
       <Layout>
-        <h1>Crear cuenta</h1>
+        <>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}>
+              Crear cuenta
+            </h1>
+          <Form>
+            <InputContainer>
+              <label
+                htmlFor="name">
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="Nombre"
+                  name="name"
+                />
+            </InputContainer>
+            <InputContainer>
+              <label
+                htmlFor="email">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Email"
+                  name="email"
+                />
+            </InputContainer>
+            <InputContainer>
+              <label
+                htmlFor="password">
+                  Contraseña
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Contraseña"
+                  name="password"
+                />
+            </InputContainer>
+            <InputSubmit
+              type="submit"
+              value="Crear cuenta"
+            />
+          </Form>
+        </>
       </Layout>
     </div>
   )
