fe52b15876359f27ef07e626f04a68ed03e1c21e
Mauro <molinamauro2@gmail.com>
2020-08-21 05:08:04 -0300

    add firebase dependencies and config

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcHJeGrL2TeJaO2TNUjIJCn4LcoLK0uIs",
+    authDomain: "mmhunt-react.firebaseapp.com",
+    databaseURL: "https://mmhunt-react.firebaseio.com",
+    projectId: "mmhunt-react",
+    storageBucket: "mmhunt-react.appspot.com",
+    messagingSenderId: "225252125857",
+    appId: "1:225252125857:web:a48639cc3b92443b1494e0"
+  };
+
+export default firebaseConfig;
