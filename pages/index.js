9d8a855515ac94c99ebec0cf0bb0cf0c62a272fc
Mauro <molinamauro2@gmail.com>
2020-08-24 18:34:21 -0300

    add product detail component and list products functionality

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import Layout from '../components/layout/Layout';
+import {FirebaseContext} from '../firebase';
+import ProductDetails from '../components/layout/ProductDetails';
 
 const Home = () => {
+
+  const [products, setProducts] = useState([]);
+
+  const {firebase} = useContext(FirebaseContext);
+
+  useEffect( () => {
+    const getProducts = () => {
+      firebase.db.collection('products').orderBy('creationDate', 'desc').onSnapshot(handleSnapshot);
+    }
+    getProducts();
+  }, [])
+
+  function handleSnapshot(snapshot){
+    const products = snapshot.docs.map( doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    });
+
+    setProducts(products);
+  }
+
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              { products.map( product => (
+                <ProductDetails
+                  key={product.id}
+                  product={product}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   )
